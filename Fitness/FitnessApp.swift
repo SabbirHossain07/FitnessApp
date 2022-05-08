ae7e8c586a68071ecccc68c4a16cb4fb9185cbae
SabbirHossain07 <sabbirsopnil07@gmail.com>
2022-05-08 23:32:53 +0600

    Initial Commit

---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Sopnil Sohan on 8/5/22.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
